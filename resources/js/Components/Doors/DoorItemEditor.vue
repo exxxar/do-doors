fd7deefa60a3e633c9cd0d5524097fe6f730f567
exxxar <exxxar@gmail.com>
2025-04-16 01:01:43 +0300

    update

--- resources/js/Components/Doors/DoorItemEditor.vue
+++ resources/js/Components/Doors/DoorItemEditor.vue
@@ -1693,8 +1693,8 @@ export default {
 
         },
         swapDoorstepStopper(index) {
-            this.doorForm.need_automatic_doorstep = index === 0
-            this.doorForm.need_hidden_stopper =  index !== 0
+            this.doorForm.need_automatic_doorstep = index === 0 && ! this.doorForm.need_automatic_doorstep
+            this.doorForm.need_hidden_stopper =  index !== 0 && !this.doorForm.need_hidden_stopper
         },
         getServiceByType(type) {
             if ((this.getDictionary.services || []).length === 0)
