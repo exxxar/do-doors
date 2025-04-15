99fece84e80245e3d599d2505f97861cd888ad07
exxxar <exxxar@gmail.com>
2025-04-16 01:18:03 +0300

    update

--- resources/js/Components/Doors/DoorItemEditor.vue
+++ resources/js/Components/Doors/DoorItemEditor.vue
@@ -516,7 +516,7 @@ import WrapperSearchModal from "@/Components/Admin/Handles/WrapperSearchModal.vu
                         <input class="form-check-input"
                                type="checkbox"
                                @change="swapDoorstepStopper(0)"
-
+                               v-model="doorForm.need_automatic_doorstep"
                                role="switch" id="need-automatic-doorstep" checked>
                         <label class="form-check-label" for="need-automatic-doorstep">
                             Нужен автоматический порог \ не нужен
@@ -548,7 +548,7 @@ import WrapperSearchModal from "@/Components/Admin/Handles/WrapperSearchModal.vu
                         <input class="form-check-input"
                                type="checkbox" role="switch"
                                @change="swapDoorstepStopper(1)"
-
+                               v-model="doorForm.need_hidden_stopper"
                                id="need-hidden-stopper" checked>
                         <label class="form-check-label" for="need-hidden-stopper">
                             Нужен скрытый стопор \ не нужен
@@ -1693,22 +1693,8 @@ export default {
 
         },
         swapDoorstepStopper(index) {
-
-            if (index === 0) {
-                if (this.doorForm.need_automatic_doorstep) {
-                    this.doorForm.need_automatic_doorstep = false;
-                } else {
-                    this.doorForm.need_automatic_doorstep = true;
-                    this.doorForm.need_hidden_stopper = false;
-                }
-            } else if (index === 1) {
-                if (this.doorForm.need_hidden_stopper) {
-                    this.doorForm.need_hidden_stopper = false;
-                } else {
-                    this.doorForm.need_hidden_stopper = true;
-                    this.doorForm.need_automatic_doorstep = false;
-                }
-            }
+            this.doorForm.need_automatic_doorstep = this.doorForm.need_automatic_doorstep ? index === 0 : !this.doorForm.need_automatic_doorstep
+            this.doorForm.need_hidden_stopper = this.doorForm.need_hidden_stopper ? index === 1 : !this.doorForm.need_hidden_stopper
         },
         getServiceByType(type) {
             if ((this.getDictionary.services || []).length === 0)
