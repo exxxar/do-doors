fa50ed2926862045286b170381f1f1684616ccc1
exxxar <exxxar@gmail.com>
2025-06-30 01:57:57 +0300

    update

--- resources/views/export/cart-part2.blade.php
+++ resources/views/export/cart-part2.blade.php
@@ -122,7 +122,7 @@
 <table border="1" cellpadding="5" cellspacing="0" width="100%">
     <tr>
         <th colspan="5" style="text-align: left;font-weight: bold;"><h3>ПРОДАВЕЦ:</h3></th>
-        <th colspan="4" style="text-align: left;font-weight: bold;"><h3>ПОКУПАТЕЛЬ:</h3></th>
+        <th colspan="5" style="text-align: left;font-weight: bold;"><h3>ПОКУПАТЕЛЬ:</h3></th>
     </tr>
 
 , , , ,
@@ -130,67 +130,67 @@
         <td colspan="2" ><strong>Имя:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_title"]}}</td>
         <td colspan="1"><strong>Имя:</strong> </td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_title"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_title"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>ИНН:</strong></td>
         <td colspan="3" style="text-align: right;">&#8203;{{$seller["seller_inn"]}}</td>
         <td colspan="1"><strong>ИНН:</strong></td>
-        <td colspan="3" style="text-align: right;">&#8203;{{$buyer["buyer_inn"]}}</td>
+        <td colspan="4" style="text-align: right;">&#8203;{{$buyer["buyer_inn"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>КПП:</strong></td>
         <td colspan="3" style="text-align: right;">&#8203;{{$seller["seller_kpp"]}}</td>
         <td colspan="1"><strong>КПП:</strong></td>
-        <td colspan="3" style="text-align: right;">&#8203;{{$buyer["buyer_kpp"]}}</td>
+        <td colspan="4" style="text-align: right;">&#8203;{{$buyer["buyer_kpp"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>ОГРНИП:</strong></td>
         <td colspan="3" style="text-align: right;">&#8203;{{$seller["seller_ogrn"]}}</td>
         <td colspan="1"><strong>ОГРН:</strong></td>
-        <td colspan="3" style="text-align: right;">&#8203;{{$buyer["buyer_ogrn"]}}</td>
+        <td colspan="4" style="text-align: right;">&#8203;{{$buyer["buyer_ogrn"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>Юр. адрес:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_legal_address"]}}</td>
         <td colspan="1"><strong>Юр. адрес:</strong></td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_legal_address"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_legal_address"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>Банк:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_bank_name"]}}</td>
         <td colspan="1"><strong>Банк:</strong></td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_bank_name"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_bank_name"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>БИК:</strong></td>
         <td colspan="3" style="text-align: right;">&#8203;{{$seller["seller_bank_bic"]}}</td>
         <td colspan="1"><strong>БИК:</strong></td>
-        <td colspan="3" style="text-align: right;">&#8203;{{$buyer["buyer_bank_bic"]}}</td>
+        <td colspan="4" style="text-align: right;">&#8203;{{$buyer["buyer_bank_bic"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>к/сч:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_correspondent_account"]}}</td>
         <td colspan="1"><strong>к/сч:</strong></td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_correspondent_account"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_correspondent_account"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>р/сч:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_checking_account"]}}</td>
         <td colspan="1"><strong>р/сч:</strong></td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_checking_account"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_checking_account"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>Телефон:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_phone"]}}</td>
         <td colspan="1"><strong>Телефон:</strong></td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_phone"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_phone"]}}</td>
     </tr>
     <tr>
         <td colspan="2"><strong>Email:</strong></td>
         <td colspan="3" style="text-align: right;">{{$seller["seller_email"]}}</td>
         <td colspan="1"><strong>Email:</strong></td>
-        <td colspan="3" style="text-align: right;">{{$buyer["buyer_email"]}}</td>
+        <td colspan="4" style="text-align: right;">{{$buyer["buyer_email"]}}</td>
     </tr>
 </table>
 
