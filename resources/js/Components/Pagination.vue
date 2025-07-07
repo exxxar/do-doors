afc6221613962c973b1b182cdb1e1c35588f6eba
exxxar <exxxar@gmail.com>
2025-07-08 00:11:38 +0300

    update

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,113 +1,129 @@
 <template>
-    <div class="row d-flex justify-content-center" v-if="pagination">
 
-        <div class="col-lg-6 dt-pagination d-flex justify-content-center align-items-center" v-if="pagination.links">
 
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item ">
-                        <a class="page-link rounded-0"
-                           v-bind:class="{'disabled':pagination.links.prev===null}"
-                           @click="prevPage" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    <li class="page-item"
-                        :key="'paginate'+index"
-                        v-for="(item, index) in filteredLinks"
-                        @click="page(index)"
-                        v-bind:class="{'active-dark':index===pagination.meta.current_page }">
-                        <a class="page-link rounded-0"
-                            v-bind:class="{'text-secondary':index!==pagination.meta.current_page}"
-                           v-if="index!==0&&index!==filteredLinks.length-1"
-                           href="#">{{item.label}}</a>
-                    </li>
+    <nav v-if="pagination.links"  class="mt-4">
+        <ul class="pagination pagination- justify-content-center mb-3">
 
-                    <li class="page-item">
-                        <a class="page-link rounded-0 text-secondary"
-                           v-bind:class="{'disabled':pagination.links.next===null}"
-                           @click="nextPage"
-                           aria-label="Next ">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+            <li class="page-item">
+                <button
+                    type="button"
+                    @click="first"
+                    :disabled="pagination.links.first==null||pagination.meta.current_page === 1"
+                    v-bind:class="{'bg-gray2-dark':pagination.links.first===null||pagination.meta.current_page === 1,'bg-black':pagination.links.first!=null}"
+                    class="page-link rounded-xs color-white  shadow-xl border-0" tabindex="-1"
+                    aria-disabled="true"><i class="fa-solid fa-angles-left"></i></button>
+            </li>
 
-        </div>
+            <li class="page-item">
+                <button
+                    type="button"
+                    @click="prevPage"
+                    :disabled="pagination.links.prev==null"
+                    v-bind:class="{'bg-gray2-dark':pagination.links.prev==null,'bg-black':pagination.links.prev!=null}"
+                    class="page-link rounded-xs color-white shadow-xl border-0" tabindex="-1"
+                    aria-disabled="true"><i class="fa fa-angle-left"></i>
+                </button>
+            </li>
 
-    </div>
+
+
+            <li class="page-item">
+                <button
+                    @click="nextPage"
+                    :disabled="pagination.links.next==null"
+                    v-bind:class="{'bg-gray2-dark':pagination.links.next==null,'bg-black':pagination.links.next!=null}"
+                    type="button"
+                    class="page-link rounded-xs color-white  shadow-xl border-0"><i
+                    class="fa fa-angle-right"></i></button>
+            </li>
+
+            <li class="page-item">
+                <button
+                    @click="last"
+                    :disabled="pagination.links.last==null||pagination.meta.current_page === pagination.meta.last_page"
+                    v-bind:class="{'bg-gray2-dark':pagination.links.last==null||pagination.meta.current_page === pagination.meta.last_page,'bg-black':pagination.links.last!=null}"
+                    type="button"
+                    class="page-link rounded-xs color-white  shadow-xl border-0">
+                    <i class="fa-solid fa-angles-right"></i>
+                </button>
+            </li>
+        </ul>
+    </nav>
+    <p class="text-center mb-3" v-if="pagination.links" >
+        <small style="font-weight: bold;">
+            Страница {{pagination.meta.current_page}} из {{pagination.meta.last_page}}
+        </small>
+    </p>
 </template>
 <script>
 
 
 export default {
-    props:["pagination"],
-    data(){
+    props: ["pagination"],
+    data() {
         return {
-            currentPage:1,
+            currentPage: 1,
         }
     },
     computed: {
-        hasPagination(){
-            if (this.pagination===null||!this.pagination)
+        hasPagination() {
+            if (this.pagination === null || !this.pagination)
                 return false;
 
-            if (this.pagination.meta.links[0].active===false
-                &&this.pagination.meta.links[this.pagination.meta.links.length-1].active===false)
+            if (this.pagination.meta.links[0].active === false
+                && this.pagination.meta.links[this.pagination.meta.links.length - 1].active === false)
                 return false
             return true;
         },
-        filteredLinks(){
+        filteredLinks() {
             if (!this.pagination)
                 return [];
 
             let index = parseInt(this.pagination.meta.current_page)
 
+
             return this.pagination.meta.links
         }
     },
 
-    methods:{
-        nextPage(){
-            this.currentPage = this.pagination.meta.current_page+1
-            this.$emit('pagination_page', this.pagination.meta.current_page+1)
+    methods: {
+        first() {
+            this.$emit('pagination_page', 1)
         },
-        page(index){
+        last() {
+            this.$emit('pagination_page', this.pagination.meta.last_page)
+        },
+        nextPage() {
+            this.currentPage = this.pagination.meta.current_page + 1
+            this.$emit('pagination_page', this.pagination.meta.current_page + 1)
+        },
+        page(index) {
             this.currentPage = index
             /*if (this.currentPage===index)
                 return;*/
 
             window.scrollTo({
-                top: 500,
+                top: 10,
                 behavior: "smooth"
             })
 
-
-
             this.$emit('pagination_page', index)
         },
-        prevPage(){
-            if (this.currentPage===1)
+        prevPage() {
+            if (this.currentPage === 1)
                 return
 
-            this.currentPage = this.pagination.meta.current_page-1
-            this.$emit('pagination_page', this.pagination.meta.current_page-1)
+            this.currentPage = this.pagination.meta.current_page - 1
+            this.$emit('pagination_page', this.pagination.meta.current_page - 1)
         }
     },
 
 }
 </script>
-<style lang="scss">
+<style>
 .page-item {
     height: 100%;
 }
 
-.active-dark {
-    .page-link {
-        background-color: black !important;
-        color:white;
-    }
 
-}
 </style>
