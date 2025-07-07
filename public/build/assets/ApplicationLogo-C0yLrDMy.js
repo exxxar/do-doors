130e410b71dd440284b42254b616a9f48cb59a24
exxxar <exxxar@gmail.com>
2025-07-07 23:51:32 +0300

    update

---
+++ public/build/assets/ApplicationLogo-C0yLrDMy.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,c as e}from"./app-C4M7Oqyd.js";const t={},r={src:"/images/logo.png",alt:""};function n(s,_){return c(),e("img",r)}const m=o(t,[["render",n]]);export{m as A};
